0877517ab3502e7d936d0e7f36903ad430aff7ed
Kanak Tanwar <kanaktanwarpro@gmail.com>
2024-07-07 18:55:42 +0530

    setup mindsdb

---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+from dotenv import load_dotenv
+import mindsdb_sdk
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+WEAVIATE_CLUSTER_URL = os.getenv("WEAVIATE_CLUSTER_URL")
+WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
+
+conn = mindsdb_sdk.connect("http://localhost:47334")
+
+project_query = "CREATE PROJECT IF NOT EXISTS gitbot;"
+
+ml_engine_query = f"""
+CREATE ML_ENGINE IF NOT EXISTS gitbot.openai_engine
+FROM openai
+USING
+    openai_api_key = '{OPENAI_API_KEY}'
+"""
+
+llm_query = """
+CREATE MODEL IF NOT EXISTS gitbot.openai_llm
+PREDICT response
+USING
+    engine = 'openai_engine',
+    model_name = 'gpt-4o',
+    prompt_template = '',
+    temperature = 0
+"""
+
+embeddings_query = """
+CREATE MODEL IF NOT EXISTS gitbot.gemini_embeddings
+PREDICT response
+USING 
+    engine = 'openai_engine',
+    mode = 'embedding',
+    model_name = 'text-embedding-ada-002',
+    question_column = 'content'
+"""
+
+# weaviate_query = f"""
+# CREATE DATABASE IF NOT EXISTS vector_store
+# WITH ENGINE = "weaviate",
+#     PARAMETERS = {{
+#         "weaviate_url": "{WEAVIATE_CLUSTER_URL}",
+#         "weaviate_api_key": "{WEAVIATE_API_KEY}"
+#     }};
+# """
+
+# Executing above queries
+conn.query(project_query).fetch()
+conn.query(ml_engine_query).fetch()
+conn.query(llm_query).fetch()
+conn.query(embeddings_query).fetch()
+# conn.query(weaviate_query).fetch()
