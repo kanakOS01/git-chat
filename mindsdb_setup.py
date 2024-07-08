fea4917bd6bc2ecbf81307d307b3f224d93a1d67
Kanak Tanwar <kanaktanwarpro@gmail.com>
2024-07-08 20:15:40 +0530

    Update mindsdb_setup.py

--- mindsdb_setup.py
+++ mindsdb_setup.py
@@ -2,7 +2,7 @@ import mindsdb_sdk
 
 conn = mindsdb_sdk.connect("http://localhost:47334")
 gitbot = conn.get_project('gitbot')
-llm = gitbot.get_model('openai_llm')
+llm = gitbot.get_model('gemini_llm')
 
 def query(context, question):
     prompt = "Use the following pieces of context belonging to a github repo to answer the question at the end.\nIf you don't know the answer, just say that you don't know, don't try to make up an answer.\nAssume the user is a beginner and explain things properly.\nMention the source of the context at the end of the answer.\n\n{{context}}\n\nQuestion: {{question}}\n\nHelpful Answer: ",
